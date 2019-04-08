62a2774faed2123ceb1cf11223ddb80817f2a6d9
mrchelli <lukas.kellenberger@hsr.ch>
2019-04-08 17:44:33 +0200

    activate service by button

---
+++ Readinizer.Backend.Business/Interfaces/IADRSoPService.cs
@@ -0,0 +1,14 @@
+﻿using Readinizer.Backend.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Readinizer.Backend.Business.Interfaces
+{
+    public interface IADRSoPService
+    {
+        
+    }
+}
