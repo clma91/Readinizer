a6a2eb509be9983dce5f9a031168b9cf0b4d5643
mrchelli <lukas.kellenberger@hsr.ch>
2019-04-15 09:26:45 +0200

    Rsop service, interface and dll

--- Readinizer.Backend.Business/Services/ADRSoPService.cs
+++ Readinizer.Backend.Business/Services/ADRSoPService.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.DirectoryServices;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Readinizer.Backend.Business.Interfaces;
+using Readinizer.Backend.DataAccess.Interfaces;
+using Readinizer.Backend.Domain.Models;
+using Microsoft.GroupPolicy;
+
 
 namespace Readinizer.Backend.Business.Services
 {
-    class ADRSoPService
+    public class ADRSoPService : IADRSoPService
     {
+        public void getRSoP()
+        {
+
+            GPRsop test = new GPRsop(RsopMode.Logging, "");
+            test.LoggingComputer = "sysadmWS01";
+            test.LoggingUser = "readinizer.ch\\lkellenb";
+            test.LoggingMode = LoggingMode.Computer;
+            test.CreateQueryResults();
+            test.GenerateReportToFile(ReportType.Xml, "C:\\Users\\lkellenb\\Desktop\\test.xml");
+
+        }
+
     }
 }
